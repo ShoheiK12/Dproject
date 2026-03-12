bbcc7e1ff6832e9ea4da356907dd42ba7445c883
ShoheiK12 <shohei.kotera22@gmail.com>
2026-03-12 18:41:47 +1100

    update portfolio page (index.html and base.js) and add images folder

--- dproject/static/portfolio/js/base.js
+++ dproject/static/portfolio/js/base.js
@@ -1,20 +1,14 @@
-/* 最初にアニメーションが表示される（そのほかは背景色(黒色)で隠されている）。そのあと、Javascriptのwindow.onloadが発火し、3秒待った後に、bodyにloadedクラスが追加され、CSSのbody.loaded .animationのCSSが動く、つまり、アニメーションのopacityは0(フェードアウト)になり、アニメーションはhiddenされる。その後、CSSのtransition: opacity 0.8s ease, visibility 0.8s ease;により、0.8 秒かけて自然に消える。 */
-window.onload = function () {
-    setTimeout(function () {
-      document.body.classList.add('loaded');
-    }, 3000); 
-  };
-
 /* Light and Dark mode*/
 var icon = document.querySelector("#icon");
 
 icon.onclick = () => {
   document.body.classList.toggle("dark-mode");
+
   if (document.body.classList.contains("dark-mode")) {
-    icon.src = "Image/sun.png";
-    } else {
-    icon.src = "Image/moon.png";
-    }
+    icon.src = icon.dataset.sun;
+  } else {
+    icon.src = icon.dataset.moon;
+  }
 };
 
 /* Starfall animation */
